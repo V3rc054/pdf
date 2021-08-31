df3ed787b5731fddcd633f40eb3ad0ff9b1224dc
Vercosa <contatovercosa@gmail.com>
2021-08-30 23:26:40 -0300

    finalize form bsade

--- src/pdf/pdf_bsade.js
+++ src/pdf/pdf_bsade.js
@@ -2,34 +2,47 @@ const PDFDocument = require('pdfkit');
 const fs = require('fs');
 
 const bsade = {
-nome : 'André',
-   endereco : 'Rua Benedita Nunes de Campos',
-   cidade : 'Sao Jose do Rio Preto',
-   bairro : 'Parque Novo Horizonte',
-   cep : '12239008',
-   rgcnh : '412483623',
-   o_emissor : 'SSP',
-   dateEmissao : '10/10/2005',
-   cpfCnpj : '32922454860',
-   tel : '01239669595',
-   cel : '01239669595',
-   email : 'contatovercosa@gmail.com',
-   nf : 41236958545365,
-   dateVenda : '10/05/2006',
-   localVenda : 'São José dos Campos',
-   vendedor : 'Maria da Silva',
-   cpf_cnpjVendedor : '27018085000191',
-   nomeEmbarcacao : 'Trovao do Mar',
-   incricao : 236985225541252,
-   arqBruta : '052053',
-   tipo : 'Veleiro',
-   arqLiquida : '080560',
-   atividade : 'Recreacao',
-   comprimento : 250,
-   tripulantes : 02,
-   anoConstrucao  :'10/06/2001',
-   boca : '12345',
-   pontal : '012456'
+  nome: 'André',
+  endereco: 'Rua Benedita Nunes de Campos',
+  cidade: 'Sao Jose do Rio Preto',
+  bairro: 'Parque Novo Horizonte',
+  cep: '12239008',
+  rgcnh: '412483623',
+  o_emissor: 'SSP',
+  dateEmissao: '10/10/2005',
+  cpfCnpj: '32922454860',
+  tel: '01239669595',
+  cel: '01239669595',
+  email: 'contatovercosa@gmail.com',
+  nf: 41236958545365,
+  dateVenda: '10/05/2006',
+  localVenda: 'São José dos Campos',
+  vendedor: 'Maria da Silva',
+  cpf_cnpjVendedor: '27018085000191',
+  nomeEmbarcacao: 'Trovao do Mar',
+  incricao: 236985225541252,
+  arqBruta: '052053',
+  tipo: 'Veleiro',
+  arqLiquida: '080560',
+  atividade: 'Recreacao',
+  comprimento: 250,
+  tripulantes: 02,
+  anoConstrucao: '10/06/2001',
+  boca: '12345',
+  pontal: '012456',
+  passageiros: '000000',
+  numCasco: '000000',
+  matCasco: '000000',
+  contorno: '000000',
+  oneMotorMarca: '000000',
+  onePotencia: '0000000',
+  oneNumSerie: '000000000',
+  twoMotorMarca: '000000',
+  twoPotencia: '0000000',
+  twoNumSerie: '000000000',
+  threeMotorMarca: '000000',
+  threePotencia: '0000000',
+  threeNumSerie: '000000000'
 }
 
 //CRIA O DOCUMENTO
@@ -49,7 +62,7 @@ doc
   //DADOS PESSOAIS DO REQUERENTE
   .fontSize(11)
   .text(bsade.nome, 195, 214)
-  
+
   .text(bsade.endereco, 120, 231)
   .text(bsade.cidade, 100, 250)
   .text(bsade.bairro, 280, 250)
@@ -79,9 +92,23 @@ doc
   .text(bsade.atividade, 320, 575)
   .text(bsade.comprimento, 125, 590)
   .text(bsade.tripulantes, 290, 590)
-  .text(bsade.anoConstrucao, 475, 590, {lineBreak: false})
+  .text(bsade.anoConstrucao, 475, 590, { lineBreak: false })
   .text(bsade.boca, 145, 605)
   .text(bsade.pontal, 145, 620)
+  .text(bsade.passageiros, 290, 605)
+  .text(bsade.numCasco, 440, 605)
+  .text(bsade.matCasco, 270, 620)
+  .text(bsade.contorno, 475, 620)
+  .text(bsade.oneMotorMarca, 145, 650)
+  .text(bsade.onePotencia, 315, 650)
+  .text(bsade.oneNumSerie, 430, 650)
+  .text(bsade.twoMotorMarca, 145, 665)
+  .text(bsade.twoPotencia, 315, 665)
+  .text(bsade.twoNumSerie, 430, 665)
+  .text(bsade.threeMotorMarca, 145, 680)
+  .text(bsade.threePotencia, 315, 680)
+  .text(bsade.threeNumSerie, 430, 680)
+
 
 doc.end()
 
