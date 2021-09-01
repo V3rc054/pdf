329fe009443f12f082b3a16d8d61b3ab6904dc57
Vercosa <contatovercosa@gmail.com>
2021-09-01 00:08:08 -0300

    Extravio CHA ok

--- src/pdf/pdf_extravioCHA.js
+++ src/pdf/pdf_extravioCHA.js
@@ -14,35 +14,11 @@ const extravioCHA = {
     tel: '01239669595',
     cel: '01239669595',
     email: 'contatovercosa@gmail.com',
-    nf: 41236958545365,
-    dateVenda: '10/05/2006',
-    localVenda: 'São José dos Campos',
-    vendedor: 'Maria da Silva',
-    cpf_cnpjVendedor: '27018085000191',
-    nomeEmbarcacao: 'Trovao do Mar',
-    incricao: 236985225541252,
-    arqBruta: '052053',
-    tipo: 'Veleiro',
-    arqLiquida: '080560',
-    atividade: 'Recreacao',
-    comprimento: 250,
-    tripulantes: 02,
-    anoConstrucao: '10/06/2001',
-    boca: '12345',
-    pontal: '012456',
-    passageiros: '000000',
-    numCasco: '000000',
-    matCasco: '000000',
-    contorno: '000000',
-    oneMotorMarca: '000000',
-    onePotencia: '0000000',
-    oneNumSerie: '000000000',
-    twoMotorMarca: '000000',
-    twoPotencia: '0000000',
-    twoNumSerie: '000000000',
-    threeMotorMarca: '000000',
-    threePotencia: '0000000',
-    threeNumSerie: '000000000'
+    numCHA: '000000000000',
+    dateEmissaoCha: '000022223233',
+    categoria: '1212122121212',
+    numero: '4500',
+    complemento: 'Casa 8',
 }
 
 //CRIA O DOCUMENTO
@@ -72,11 +48,16 @@ doc
     .text(extravioCHA.o_emissor, 275, 140)
     .text(extravioCHA.dateEmissao, 430, 139)
     .text(extravioCHA.cpfCnpj, 80, 161)
-    .text(extravioCHA.tel, 260, 287)
+    //.text(extravioCHA.tel, 260, 287)
     .text(extravioCHA.cha, 435, 287)
-    .text(extravioCHA.dateEmissaoCha, 95, 305)
-    .text(extravioCHA.categoria, 95, 305)
-  
+    //.text(extravioCHA.dateEmissaoCha, 95, 305)
+   // .text(extravioCHA.categoria, 95, 305)
+    .text(extravioCHA.tel, 130, 228)
+    .text(extravioCHA.numCHA, 130, 250)
+    .text(extravioCHA.dateEmissaoCha, 355, 250)
+    .text(extravioCHA.categoria, 95, 270)
+    .text(extravioCHA.numero, 80, 183)
+    .text(extravioCHA.complemento, 120, 183)
 
     
 
