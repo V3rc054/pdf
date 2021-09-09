c8331eefff9230d7e649bd0e1f60415962a331e2
vercosa <contatovercosa@gmail.com>
2021-09-09 09:47:26 -0300

    completion extravio norman03/initial re_armador

--- src/config/app.js
+++ src/config/app.js
@@ -2,9 +2,9 @@ const pdf = require("../pdf/pdf_bsade")
 const pdf_extravioCHA = require("../pdf/pdf_extravioCHA")
 const pdf_declaracaoRes = require("../pdf/pdf_declaracaoRes")
 const pdf_procuracao = require('../pdf/pdf_procuracao')
+const extravioNorman03 = require('../pdf/pdf_extravioNorman03')
 
-
-module.exports = pdf,pdf_extravioCHA, pdf_declaracaoRes, pdf_procuracao
+module.exports = pdf, pdf_extravioCHA, pdf_declaracaoRes, pdf_procuracao, extravioNorman03
 
 
 
